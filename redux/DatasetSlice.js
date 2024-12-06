251fddb2d931dad8c7820df5988a6247f08acd6b
Ferraah <daniele6.ferrario@mail.polimi.it>
2024-12-06 13:34:26 +0000

    Add redux.

---
+++ redux/DatasetSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+//import Papa from "papaparse"
+
+// get the data in asyncThunk
+export const fetchFileData = createAsyncThunk('seoulBikeData/fetchData', async () => {
+    const response = await fetch('data/SeoulBikeData.csv');
+    const responseText = await response.text();
+    console.log("loaded file length:" + responseText.length);
+    const responseJson = Papa.parse(responseText,{header:true, dynamicTyping:true});
+    //return responseJson.data.map((item,i)=>{return {...item,index:i}}).slice(0, -7000); // 
+    //return responseJson.data.map((item,i)=>{return {...item,index:i}}).slice(0, 10); // 
+    return responseJson.data.map((item,i)=>{return {...item,index:i}});
+    // when a result is returned, extraReducer below is triggered with the case setSeoulBikeData.fulfilled
+})
+
+const prepareInitialState = (data) => {
+
+    return {
+    }
+}
+
+export const stateSlice = createSlice({
+  name: 'state',
+  initialState: {},
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchFileData.fulfilled, (state, action) => {
+      // Add any fetched house to the array
+      return prepareInitialState(action.payload)
+    })
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const {  updateAxisAttributes, updateSelectedItemsIndices} = stateSlice.actions
+
+export default stateSlice.reducer
